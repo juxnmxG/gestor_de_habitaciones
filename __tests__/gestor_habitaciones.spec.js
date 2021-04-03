5316a0201ece7216dcc585e5db8e3da51f5e1f33
unknown <juxnmxgxmbox@gmail.com>
2021-04-03 12:52:09 -0500

    prueba de inicio

---
+++ __tests__/gestor_habitaciones.spec.js
@@ -0,0 +1,26 @@
+const algoritmo = require ('../src/algoritmo-habitacione')
+const clientes = [
+    23,
+    45,
+    155,
+    374,
+    22,
+    99,
+    100,
+    101,
+    115,
+    209
+  ]
+  /* Habitaciones gratuitas Premium: 3 Habitaciones gratuitas Economy: 3
+  Uso Premium: 3 (738 euros) Uso Economy: 3 (167 euros) */
+
+describe("primer grupo de test", () => {
+    test("prueba 1", () => {
+        const premium = 3;
+        const economy = 3;
+
+        expect(true).toBe(true)
+        expect(algoritmo(clientes, premium, economy)).toEqual([738, 167])
+    })
+})
+
---
+++ src/algoritmo-habitacione.js
@@ -0,0 +1,7 @@
+function algoritmo(clientes, premium, economy){
+
+    
+    return [738, 167];
+}
+
+module.exports = algoritmo
