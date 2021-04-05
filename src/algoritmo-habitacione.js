028830b6413babf28d91feb9678d28f5dc32c7e9
juxnmxG <juxnmxgxmbox@gmail.com>
2021-04-04 20:58:23 -0500

    prueba1

--- src/algoritmo-habitacione.js
+++ src/algoritmo-habitacione.js
@@ -1,8 +1,37 @@
 function algoritmo(clientes, premium, economy){
 
-    asdassa
-    
-    return [738, 167];
+    clientes.sort();
+    const mayores = [];
+    const menores = [];
+    const result = [];
+
+    for (var i = 0; i < clientes.length; i++ ){
+
+        if (clientes[i] >= 100){
+            mayores.push(clientes[i]);
+        }else{
+            menores.push(clientes[i]);
+        }
+    }
+
+    const pre = suma(mayores);
+    const eco = suma(menores);
+
+    result.push(pre);
+    result.push(eco);
+
+    return result;
+}
+function suma(valores){
+
+    valores.sort();
+    let sum = 0;
+
+    for(var i = valores.length - 3; i < valores.length; i++ ){
+        sum = sum + valores[i];
+    }
+
+    return sum;
 }
 
 module.exports = algoritmo
