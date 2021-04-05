06d7477ff6fbff6b7099c88937f2aa418ae16ffb
juxnmxG <juxnmxgxmbox@gmail.com>
2021-04-05 00:06:40 -0500

    algoritmo completo

--- src/algoritmo-habitacione.js
+++ src/algoritmo-habitacione.js
@@ -14,20 +14,42 @@ function algoritmo(clientes, premium, economy){
         }
     }
 
-    const pre = suma(mayores);
-    const eco = suma(menores);
+    let pre = 0; 
+    let eco = 0;
+
+    //pregunto que si las habitaciones premium estan en su totalidad ocupadas
+    if (mayores.length == premium  || mayores.length > premium){
+        pre = suma(mayores, premium);
+        if(menores.length > economy){
+            eco = suma(menores, economy);
+        }else{
+            eco = suma(menores, menores.length);
+        }
+        
+    }else{//pregunto si las habitaciones economy estan todas ocupadas
+        if(menores.length < economy || menores.length == economy){
+            pre = suma(mayores, mayores.length);
+            eco = suma(menores, menores.length)
+        }else{
+            const cal = premium - mayores.length; 
+            pre = (suma(mayores, premium - cal)) + suma(menores, cal);
+            menores.splice(menores.length - cal, cal);
+            eco = suma(menores, economy);
+        }
+    }
 
     result.push(pre);
     result.push(eco);
 
     return result;
 }
-function suma(valores){
+
+function suma(valores, cantidad){
 
     valores.sort();
     let sum = 0;
 
-    for(var i = valores.length - 3; i < valores.length; i++ ){
+    for(var i = valores.length - cantidad; i < valores.length; i++ ){
         sum = sum + valores[i];
     }
 
